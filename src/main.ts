294edcab34622399a22bf49a288c7ab674909850
brandford7 <brandfordjunior@gmail.com>
2025-07-09 09:47:52 +0000

    added global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,14 @@ async function bootstrap() {
     origin: 'http://localhost:5173', // specify exact origin
     credentials: true, // allow credentials (cookies, auth headers)
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips properties that don't have decorators
+      forbidNonWhitelisted: true, // throws if unknown properties exist
+      transform: true, // transforms payloads to DTO instances
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
